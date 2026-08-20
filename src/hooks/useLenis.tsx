3db89d8a0397045fc9db4559c8eac03d9bb50ff9
DevZahidHasan <zahidhasan19932023@gmail.com>
2026-08-20 16:36:06 +0600

    chnages done

--- src/hooks/useLenis.tsx
+++ src/hooks/useLenis.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useEffect, useState } from 'react';
+import React, { createContext, useContext, useEffect } from 'react';
 import Lenis from 'lenis';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import { gsap } from '../motion/gsap';
